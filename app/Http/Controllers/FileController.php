23d4ee1e66e3993c2cff310f512a8362beee460b
Yuan <1398095376@qq.com>
2018-01-27 13:21:32 +0800

    initial

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+
+class FileController extends Controller
+{
+    //
+    public function index(){
+        return view('index');
+    }
+
+    public function uploadFile(Request $request){
+        if($request->isMethod('POST')){
+            $file = $request->file('homework');
+            if($file->isValid()){
+                $name = $file->getClientOriginalName();
+                $path = $file->getRealPath();
+               
+                $file->storeAs('public/homeworkFiles', $name);
+                return 'ok';
+            }
+        }
+    }
+
+    public function showFile(){
+        $file = Storage::files('public/homeworkFiles');
+        $str = '';
+        foreach($file as $val){
+            $str .= str_after($val, 'homeworkFiles/')."<br>";
+        }
+        return $str;
+    }
+
+    public function downloadFile(){
+        $fileName = '10.jpg';
+        return response()->download(storage_path('app/public/homeworkFiles/'.$fileName));
+    }
+}
